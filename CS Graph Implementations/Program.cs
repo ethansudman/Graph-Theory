bfda12ec3a15eab8f58780df4e93d9072a0ee842
ethansudman@comcast.net <ethansudman@comcast.net>
2015-04-03 12:37:18 -0500

    Add program

---
+++ CS Graph Implementations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CS_Graph_Implementations
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.Insert(10);
+            tree.Insert(5);
+            tree.Insert(15);
+            tree.Insert(20);
+            tree.Insert(30);
+            tree.Insert(4);
+            tree.Insert(6);
+
+            Trace.TraceInformation(tree.MyContains(tree.Root, 30).ToString());
+
+            tree.DFS();
+
+        }
+    }
+}
